c300d1ec6acc6c5da5db9110c44e7f2f368b507f
Jim Yeung <jimycodex@gmail.com>
2024-01-22 16:28:09 +0800

    add the remote controller link

---
+++ src/main/java/de/rwth/idsg/steve/web/controller/RemoteController.java
@@ -0,0 +1,34 @@
+package de.rwth.idsg.steve.web.controller;
+
+
+import de.rwth.idsg.steve.service.ChargePointHelperService;
+import de.rwth.idsg.steve.web.dto.ocpp.RemoteStartTransactionParams;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/remoteController")
+public class RemoteController {
+
+
+    private static final String START_TX_PATH = "/StartTransaction";
+    @Autowired
+    protected ChargePointHelperService chargePointHelperService;
+    @Autowired protected Ocpp16Controller ocpp16Controller;
+    protected static final String PARAMS = "params";
+    protected String getPrefix() {
+        return "op16";
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String getRemoteStartTx(Model model) {
+        ocpp16Controller.setCommonAttributesForTx(model);
+        ocpp16Controller.setActiveUserIdTagList(model);
+        model.addAttribute(PARAMS, new RemoteStartTransactionParams());
+        return getPrefix() + START_TX_PATH;
+    }
+}
+
