10d1d3d5d5a3263f2ceb9012c1b66309702a9893
Jim Yeung <jimycodex@gmail.com>
2024-01-24 13:36:58 +0800

    add the remote controller link

--- src/main/resources/webapp/WEB-INF/views/snippets/getTransactionIds.js
+++ src/main/resources/webapp/WEB-INF/views/snippets/getTransactionIds.js
@@ -1,38 +1,46 @@
 $("#chargePointSelectList").change(function() {
 	var cp = $(this).find("option:selected").text();
+
+	// First AJAX request to get transaction IDs
 	$.getJSON("${ctxPath}/manager/ajax/" + cp + "/transactionIds", function(data) {
 		console.log(data);
+
 		var options = "";
 		$.each(data, function() {
 			options += "<option value='" + this + "'>" + this + "</option>";
 		});
+
 		var select = $("#transactionId");
 		select.prop("disabled", false);
 		select.html(options);
+
+		// Check if data has at least one element before making the second AJAX request
+		if (data.length > 0) {
+			// Capture the first transactionId from the data
+			var firstTransactionId = data[0];
+
+			// Second AJAX request using the captured transactionId
+			$.getJSON("${ctxPath}/manager/ajax/" + cp + "/transactionDetails/" + firstTransactionId, function(data1) {
+				console.log(data1);
+
+				// Your logic with data1
+			});
+		}
 	});
-		$.getJSON("${ctxPath}/manager/ajax/" + cp + "/transactionDetails" + this, function(data1) {
-			console.log(data1);
-			// var options = "";
-			// $.each(data, function() {
-			// 	options += "<option value='" + this + "'>" + this + "</option>";
-			// });
-			// var select = $("#transactionId");
-			// select.prop("disabled", false);
-			// select.html(options);
-		});
 });
 
-$("#chargePointSelectList").change(function() {
-	var cp = $(this).find("option:selected").text();
-	$.getJSON("${ctxPath}/manager/ajax/" + cp + "/transactionDetails", function(data) {
-		console.log(data);
-		var options = "";
-		$.each(data, function() {
-			options += "<option value='" + this + "'>" + this + "</option>";
-		});
-		var select = $("#transactionId");
-		select.prop("disabled", false);
-		select.html(options);
-	});
-});
+
+// $("#chargePointSelectList").change(function() {
+// 	var cp = $(this).find("option:selected").text();
+// 	$.getJSON("${ctxPath}/manager/ajax/" + cp + "/transactionDetails", function(data) {
+// 		console.log(data);
+// 		var options = "";
+// 		$.each(data, function() {
+// 			options += "<option value='" + this + "'>" + this + "</option>";
+// 		});
+// 		var select = $("#transactionId");
+// 		select.prop("disabled", false);
+// 		select.html(options);
+// 	});
+// });
 
